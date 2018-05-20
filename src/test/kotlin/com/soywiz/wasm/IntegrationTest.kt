39a8135da849b036315db91bf06d9f87f2f191e3
soywiz <soywiz@gmail.com>
2018-05-20 14:07:37 +0200

    Compare output with GCC + travis:language:java

--- src/test/kotlin/com/soywiz/wasm/IntegrationTest.kt
+++ src/test/kotlin/com/soywiz/wasm/IntegrationTest.kt
@@ -6,10 +6,12 @@ import com.soywiz.korio.vfs.*
 import kotlin.test.*
 
 class IntegrationTest {
+    //val root = tempVfs
+    val root = "/tmp".uniVfs
+
     @Test
     fun testSimpleIntegrationTest() {
-        assertExecutionEquals(
-            "hello world\n0,1,2,3,4,5,6,7,8,9,\n",
+        assertGccJavaExecutionEquals(
             """
             #include <stdio.h>
             int main() {
@@ -22,7 +24,44 @@ class IntegrationTest {
         )
     }
 
-    private fun compileAndExecute(source: String): String {
+    private suspend fun runCommand(vararg args: String): String {
+        println(args.joinToString(" "))
+        return root.execToString(*args)
+    }
+
+    private fun compileAndExecuteGCC(source: String): String {
+        var result = ""
+        runBlocking {
+            root["wasm-program.c"].writeString(source)
+            println(root.absolutePath)
+
+            // C -> BIN
+            runCommand(
+                "docker",
+                "run",
+                "-v",
+                "${root.absolutePath}:/src",
+                "gcc:8.1.0",
+                "gcc",
+                "/src/wasm-program.c",
+                "-o",
+                "/src/wasm-program.out"
+            )
+
+            // Execute Native
+            result = runCommand(
+                "docker",
+                "run",
+                "-v",
+                "${root.absolutePath}:/src",
+                "gcc:8.1.0",
+                "/src/wasm-program.out"
+            )
+        }
+        return result
+    }
+
+    private fun compileAndExecuteJava(source: String): String {
         var result = ""
         runBlocking {
             //val root = tempVfs
@@ -31,28 +70,38 @@ class IntegrationTest {
             println(root.absolutePath)
 
             // C -> WASM
-            val emccArgs = arrayOf("docker", "run", "--rm", "-v", "${root.absolutePath}:/src", "-t", "apiaryio/emcc", "emconfigure", "emcc", "wasm-program.c", "-o", "wasm-program", "-O3", "-s", "WASM=1")
-            println(emccArgs.joinToString(" "))
-            root.execToString(*emccArgs)
+            runCommand(
+                "docker", "run", "--rm", "-v", "${root.absolutePath}:/src", "-t", "apiaryio/emcc",
+                "emconfigure", "emcc", "wasm-program.c", "-o", "wasm-program", "-O3", "-s", "WASM=1"
+            )
 
             // WASM -> Java
             val data = root["wasm-program.wasm"].readAll().openSync()
             root["Module.java"].writeString(Wasm.readAndConvert(data, "java").toString())
 
             // Java -> Class
-            val javacArgs = arrayOf("docker", "run", "-v", "${root.absolutePath}:/src", "jboss/base-jdk:8", "javac", "/src/Module.java")
-            println(javacArgs.joinToString(" "))
-            root.execToString(*javacArgs)
+            runCommand(
+                "docker", "run", "-v", "${root.absolutePath}:/src", "jboss/base-jdk:8",
+                "javac", "/src/Module.java"
+            )
 
             // Execute Java
-            val javaArgs = arrayOf("docker", "run", "-v", "/tmp:/src", "jboss/base-jdk:8", "java", "-cp", "/src", "Module")
-            println(javaArgs.joinToString(" "))
-            result = root.execToString(*javaArgs)
+            result = runCommand(
+                "docker", "run", "-v", "/tmp:/src", "jboss/base-jdk:8",
+                "java", "-cp", "/src", "Module"
+            )
         }
         return result
     }
 
-    private fun assertExecutionEquals(expected: String, source: String) {
-        assertEquals(expected, compileAndExecute(source), "Executing $source")
+    private fun assertGccJavaExecutionEquals(source: String) {
+        val gccOutput = compileAndExecuteGCC(source)
+        val javaOutput = compileAndExecuteJava(source)
+        println(gccOutput)
+        assertEquals(gccOutput, javaOutput, "Executing $source")
     }
+
+    //private fun assertExecutionEquals(expected: String, source: String) {
+    //    assertEquals(expected, compileAndExecuteJava(source), "Executing $source")
+    //}
 }
